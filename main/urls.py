6f60d385083f0a28bebb4a51ee374a009a2878f2
Veronika <rmrgveronica@gmail.com>
2022-12-07 22:01:59 +0300

    2 commit

--- main/urls.py
+++ main/urls.py
@@ -4,5 +4,6 @@ from . import views
 
 urlpatterns = [
     path('', views.index),
-    path('registration', views.registration)
+    path('registration', views.registration),
+    path('login', views.login)
 ]
--- main/views.py
+++ main/views.py
@@ -4,6 +4,10 @@ from django.shortcuts import render
 def index(request):
     return render(request, 'main/index.html')
 
+
 def registration(request):
     return render(request, 'main/registration.html')
-# Create your views here.
+
+def login(request):
+    return render(request, 'main/login.html')
+
