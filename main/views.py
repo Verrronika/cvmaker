c6c78542329a457737b895f6ff25f8528091fc3e
Veronika <rmrgveronica@gmail.com>
2022-12-07 18:38:59 +0300

    1 com

---
+++ main/views.py
@@ -0,0 +1,9 @@
+from django.shortcuts import render
+
+
+def index(request):
+    return render(request, 'main/index.html')
+
+def registration(request):
+    return render(request, 'main/registration.html')
+# Create your views here.
